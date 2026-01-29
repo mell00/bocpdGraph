2ce6954f547a204dc09d68061d09306614eaf8c6
Madison Ell <mellmell2550@gmail.com>
2026-01-28 20:26:47 -0800

    Add as_adjacency_matrix square base matrix validation tests

--- tests/testthat/test-graph_utils.R
+++ tests/testthat/test-graph_utils.R
@@ -3,3 +3,14 @@ test_that("as_adjacency_matrix() rejects unsupported types", {
   expect_error(as_adjacency_matrix("x"), "Unsupported `graph` type")
   expect_error(as_adjacency_matrix(data.frame(a = 1)), "Unsupported `graph` type")
 })
+
+
+test_that("as_adjacency_matrix() validates square base matrices", {
+  A <- matrix(0, 2, 3)
+  expect_error(as_adjacency_matrix(A), "must be square")
+
+  B <- matrix(c(0, 1, 0, 0), 2, 2)
+  out <- as_adjacency_matrix(B, directed = TRUE)
+  expect_true(is.matrix(out))
+  expect_equal(diag(out), c(0, 0))
+})
